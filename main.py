3a10b836ace5c968476b4af270153e836fb7d8b7
Nino Lopez <nino.lopez@spathesystems.com>
2021-02-13 01:09:20 -0500

    Welcome to the Dream, Boys.

---
+++ main.py
@@ -0,0 +1,6 @@
+# Scrape a shitload of magnet url's from torrent websites
+# Store those bad boys in a DB
+# Twilio take command `find` from text and searchs urls with elasticsearch maybe?
+# Retuen text whether it found it or not and continue if you want to download
+# Call Deluge API and download magnet url
+# Rename it and FTP it over to plex library
